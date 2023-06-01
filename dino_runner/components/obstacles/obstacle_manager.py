834be32b35b13681b89a267a597670ca9d6c6219
JooJunior <juniorhc99@hotmail.com>
2023-05-31 23:57:38 -0300

    AULA 3 Commit

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -3,7 +3,6 @@ import random
 
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.components.obstacles.bird import Bird
-from dino_runner.utils.constants import SMALL_CACTUS
 
 class ObstacleManager: #D R
     def __init__(self):
@@ -22,8 +21,12 @@ class ObstacleManager: #D R
             if game.player.dino_rect.colliderect(obstacle.rect):
                 pygame.time.delay(500)
                 game.playing = False
+                game.death_count += 1 ##
                 break
             
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
+
+    def reset_obstacles(self):
+        self.obstacles = []
